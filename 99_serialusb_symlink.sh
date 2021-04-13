f7a4dd38e1123c8c9e0b73e6bfebf252ba77022c
Runar Borge <runar@borge.nu>
2021-04-13 20:14:04 +0200

    Updated script to a working condition

--- 99_serialusb_symlink.sh
+++ 99_serialusb_symlink.sh
@@ -13,38 +13,29 @@
 # port whis will enumerate the same each time as long as as external hubs
 # are not used. 
 
-
 exec 1>>/root/usbenv 2>&1  # for debugging, redirect stdout/stderr to file
-
 # Skipping bus and hub devices
-[ "$DEVTYPE" = usb_device -a "${DEVICENAME%%:*}" = "$DEVICENAME" ] || exit 0
+if [ "$DEVTYPE" = usb_device -a "${DEVICENAME%%:*}" = "$DEVICENAME" ]; then
+  # Skipping bus and hub devices
+  exit 0
+fi
 
 # TODO:need a rewrite to support different port addresses on other devices
 # We are only interested in devices on the external port
 if ! expr "${DEVICENAME}" : "1-1\.3[.:$]" > /dev/null; then
-  #echo "Device ${DEVICENAME} not on external usb port, ignoring"
   exit 0
 fi
 
 if [ "$ACTION" = bind ]; then
-  echo "Adding device: ${DEVICE_NAME} " \
-       "action='$ACTION' " \
-       "ttyName=$(ls /sys/${DEVPATH} | grep tty) " \
-       "devicename='$DEVICENAME' " \
-       "devpath='$DEVPATH' " \
-       "product='$PRODUCT' " \
-       "type='$TYPE' " \
-       "interface='$INTERFACE'"
-
   TTY_NAME=$(ls /sys/${DEVPATH} | grep tty)
   # Ignore devices without any attached tty device
-  [ -z "${TTY_NAME}" ] || exit 0
-    
+  [ -z "${TTY_NAME}" ] && exit 0
+
   i=1  
   while [ $i -lt 100 ]; do
     newName="/dev/usbSerial${i}"
     if [ ! -c "${newName}" ]; then
-      echo "Creating Symlink for ${TTY_NAME} ${newName}"
+      logger -t hotplug "Creating Symlink for ${TTY_NAME} ${newName}"
       ln -s ${TTY_NAME} ${newName}
       break
     fi
@@ -58,7 +49,7 @@ elif [ "$ACTION" = unbind ]; then
   # all serial devices and remove symlinks pointing to non-existing
   # devices
   logger -t hotplug "Device removed ${DEVICE_NAME} ${DEVICENAME} ${DEVPATH}"
-  for f in /dev/usbSerial*; do
+  for f in $(find /dev -name "usbSerial*" -type l); do
     if [ ! -c ${f} ]; then  
       sym=$(readlink -f ${f})
 
